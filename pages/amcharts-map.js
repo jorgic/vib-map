80ff6f8fe3202e0a93747eccccd85121f5be1cd8
Radovan Jorgic <radovan.jorgic@viberate.com>
2022-10-11 12:25:11 +0200

    Menu

---
+++ pages/amcharts-map.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Link from "next/link";
+
+const AmchartsMap = () => {
+  return (
+    <div>
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+      <hr />
+    </div>
+  );
+};
+
+export default AmchartsMap;
